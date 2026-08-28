9601c5175d2e823f40056d226f33257799e53f0b
cirvet <andresascaniod@gmail.com>
2026-08-28 21:24:35 +0200

    j

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/lananifitness-web">
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- src/pages/RetosPage.tsx
+++
@@ -1,8 +0,0 @@
-import Retos from '../sections/Retos';
-import { useSEO } from '../hooks/useSEO';
-import { SEO_PAGINAS } from '../data';
-
-export default function RetosPage() {
-  useSEO(SEO_PAGINAS.retos);
-  return <Retos variant="full" />;
-}
